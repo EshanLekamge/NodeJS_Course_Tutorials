ed61cd6ddae7dc0c94104045641468a8efe28721
Eshan Lakshitha Lekamge <67872507+EshanLekamge@users.noreply.github.com>
2022-01-07 18:22:09 +0530

    middleware

--- ExpressTutorial/02-express-tutorial/app.js
+++ ExpressTutorial/02-express-tutorial/app.js
@@ -1,49 +1,25 @@
 const express = require('express')
 const app = express()
-const {products} = require('./data')
+const logger = require('./logger')
 
-app.get('/',(req,res)=>{
-    res.send('<h1>Home page</h1><a href="/api/products">Products</a>')
+// req => middleware => res
+
+app.use(logger)
+
+app.get('/', (req,res)=>{
+    res.send('Home page')
 })
 
-app.get('/api/products',(req,res)=>{
-    const newProducts = products.map((product)=>{
-        const {id,name,image} = product;
-        return {id,name,image}
-    })
-    res.json(newProducts)
+app.get('/about', (req,res)=>{
+    res.send('About page')
 })
 
-app.get('/api/products/:productID',(req,res)=>{
-
-    const {productID} = req.params;
-    const singleProduct = products.find((product)=>product.id === Number(productID))
-   
-    if(!singleProduct){
-        res.status(404).send('Product Not Found')
-    }
-
-    res.json(singleProduct)
-
+app.get('/api/products', (req,res)=>{
+    res.send('Products page')
 })
 
-app.get('/api/v1/query',(req,res)=>{
-    const {search, limit} = req.query
-    let sortedProducts = [...products]
-
-    if(search){
-        sortedProducts = sortedProducts.filter((product)=>{
-            return product.name.startsWith(search)
-        })
-    }
-    if(limit){
-    sortedProducts = sortedProducts.slice(0,Number(limit))
-    }
-    if(sortedProducts.length < 1){
-        res.status(200).send('No products matching')
-    }
-    res.status(200).json(sortedProducts)
-
+app.get('/api/items', (req,res)=>{
+    res.send('Items page')
 })
 
 app.listen(5000,()=>{
